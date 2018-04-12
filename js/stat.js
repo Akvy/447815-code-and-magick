1dac57343aaa5976062447a305e227f4a99a7744
Андрей Володченко <a.k.volodchenko@gmail.com>
2018-04-12 18:34:22 +0300

    task 2 fix part 2

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,30 @@
 'use strict';
 
+var coords = [[230, 40, 260, 30],
+  [275, 5, 290, 30],
+  [310, -5, 330, 30],
+  [350, -5, 370, 30],
+  [390, -5, 410, 30],
+  [430, -5, 450, 30],
+  [465, 5, 480, 30],
+  [500, 45, 480, 60],
+  [520, 85, 480, 105],
+  [520, 135, 480, 155],
+  [520, 185, 480, 205],
+  [520, 235, 480, 255],
+  [500, 270, 480, 280],
+  [470, 300, 460, 280],
+  [430, 320, 415, 280],
+  [390, 320, 370, 280],
+  [350, 320, 330, 280],
+  [310, 320, 290, 280],
+  [270, 310, 250, 280],
+  [230, 265, 250, 250],
+  [210, 225, 250, 205],
+  [210, 185, 250, 165],
+  [210, 145, 250, 125],
+  [210, 105, 250, 85],
+  [220, 65, 250, 55]];
 var CLOUD_START_X = 250;
 var CLOUD_START_Y = 55;
 var GAP = 10;
@@ -11,35 +36,11 @@ var BAR_WIDTH = 40;
 var BAR_HEIGHT = -150;
 var RESULT_GAP = 25;
 var MIN_OPACITY = 0.1;
-var CLOUD_1_COORDINATE = [220, 30, 250, 20];
-var CLOUD_2_COORDINATE = [265, -5, 280, 20];
-var CLOUD_3_COORDINATE = [300, -15, 320, 20];
-var CLOUD_4_COORDINATE = [340, -15, 360, 20];
-var CLOUD_5_COORDINATE = [380, -15, 400, 20];
-var CLOUD_6_COORDINATE = [420, -15, 440, 20];
-var CLOUD_7_COORDINATE = [455, -5, 470, 20];
-var CLOUD_8_COORDINATE = [490, 35, 470, 50];
-var CLOUD_9_COORDINATE = [510, 75, 470, 95];
-var CLOUD_10_COORDINATE = [510, 125, 470, 145];
-var CLOUD_11_COORDINATE = [510, 175, 470, 195];
-var CLOUD_12_COORDINATE = [510, 225, 470, 245];
-var CLOUD_13_COORDINATE = [490, 260, 470, 270];
-var CLOUD_14_COORDINATE = [460, 290, 450, 270];
-var CLOUD_15_COORDINATE = [420, 310, 405, 270];
-var CLOUD_16_COORDINATE = [380, 310, 360, 270];
-var CLOUD_17_COORDINATE = [340, 310, 320, 270];
-var CLOUD_18_COORDINATE = [300, 310, 280, 270];
-var CLOUD_19_COORDINATE = [260, 300, 240, 270];
-var CLOUD_20_COORDINATE = [220, 255, 240, 240];
-var CLOUD_21_COORDINATE = [200, 215, 240, 195];
-var CLOUD_22_COORDINATE = [200, 175, 240, 155];
-var CLOUD_23_COORDINATE = [200, 135, 240, 115];
-var CLOUD_24_COORDINATE = [200, 95, 240, 75];
-var CLOUD_25_COORDINATE = [210, 55, 240, 45];
 var CLOUD_COLOR = 'rgba(0, 0, 0, 0.7)';
 var COLOR_WHITE = '#fff';
 var COLOR_BLACK = 'black';
 var COLOR_RED = 'rgba(255, 0, 0, 1)';
+var cloudColors = [CLOUD_COLOR, COLOR_WHITE];
 var FONT_STYLE = '16px Mono';
 
 var getMaxElement = function (arr) {
@@ -68,76 +69,31 @@ window.renderStatistics = function (ctx, names, times) {
   }
 
   // Рисует контуры тени облака и облака
-  var paintCloud = function () {
-    var coords = [CLOUD_1_COORDINATE,
-      CLOUD_2_COORDINATE,
-      CLOUD_3_COORDINATE,
-      CLOUD_4_COORDINATE,
-      CLOUD_5_COORDINATE,
-      CLOUD_6_COORDINATE,
-      CLOUD_7_COORDINATE,
-      CLOUD_8_COORDINATE,
-      CLOUD_9_COORDINATE,
-      CLOUD_10_COORDINATE,
-      CLOUD_11_COORDINATE,
-      CLOUD_12_COORDINATE,
-      CLOUD_13_COORDINATE,
-      CLOUD_14_COORDINATE,
-      CLOUD_15_COORDINATE,
-      CLOUD_16_COORDINATE,
-      CLOUD_17_COORDINATE,
-      CLOUD_18_COORDINATE,
-      CLOUD_19_COORDINATE,
-      CLOUD_20_COORDINATE,
-      CLOUD_21_COORDINATE,
-      CLOUD_22_COORDINATE,
-      CLOUD_23_COORDINATE,
-      CLOUD_24_COORDINATE,
-      CLOUD_25_COORDINATE];
-    var OFFSET = GAP;
+  var renderCloud = function (figure, offset, renderColor) {
 
-    ctx.fillStyle = CLOUD_COLOR;
     ctx.beginPath();
-    ctx.moveTo(CLOUD_START_X, CLOUD_START_Y);
-    for (var i = 0; i < coords.length; i++) {
-      var mas = coords[i];
+    ctx.moveTo(CLOUD_START_X - offset, CLOUD_START_Y - offset);
+    for (var i = 0; i < figure.length; i++) {
+      var mas = figure[i];
       var coordX = mas[0];
       var coordY = mas[1];
       var coordWidth = mas[2];
       var coordHeigth = mas[3];
-
-      ctx.quadraticCurveTo(coordX + OFFSET, coordY + OFFSET, coordWidth + OFFSET, coordHeigth + OFFSET);
+      ctx.quadraticCurveTo(coordX - offset, coordY - offset, coordWidth - offset, coordHeigth - offset);
     }
-
+    ctx.fillStyle = renderColor;
     ctx.fill();
-    ctx.fillStyle = COLOR_WHITE;
-    ctx.beginPath();
-    var cloudSdadoxStartX = CLOUD_START_X - OFFSET;
-    var cloudSdadoxStartY = CLOUD_START_Y - OFFSET;
-    ctx.moveTo(cloudSdadoxStartX, cloudSdadoxStartY);
-
-    for (i = 0; i < coords.length; i++) {
-      mas = coords[i];
-      coordX = mas[0];
-      coordY = mas[1];
-      coordWidth = mas[2];
-      coordHeigth = mas[3];
-      ctx.quadraticCurveTo(coordX, coordY, coordWidth, coordHeigth);
-    }
   };
 
-  paintCloud();
-  ctx.fill();
+  for (i = 0; i < cloudColors.length; i++) {
+    renderCloud(coords, 10 * i, cloudColors[i]);
+  }
 
   // Заголовок в таблице результатов
   var TEXT_TITLE = ['Ура вы победили!', 'Список результатов:'];
 
   var writeTitle = function (arr) {
     for (i = 0; i < arr.length; i++) {
-      if (arr.length > 2) {
-        arr.length = 2;
-      }
-
       ctx.fillStyle = COLOR_BLACK;
       ctx.font = FONT_STYLE;
       ctx.textBaseline = 'hanging';
@@ -154,11 +110,15 @@ window.renderStatistics = function (ctx, names, times) {
     var opacity = +Math.random().toFixed(2) + MIN_OPACITY;
     var greenRandom = Math.round(getMinMaxRandom(255));
     var barY = PLAYER_NAME_Y - 10;
+    var barX = CLOUD_START_X + GAP + (BAR_WIDTH + GAP) * i;
+    var barHeight = (BAR_HEIGHT * times[i]) / maxTime;
+    var resultY = PLAYER_NAME_Y - RESULT_GAP + (BAR_HEIGHT * times[i]) / maxTime;
 
     ctx.fillStyle = names[i] === 'Вы' ? COLOR_RED : 'rgba(0, ' + greenRandom + ', 255, ' + opacity + ')';
-    ctx.fillRect(CLOUD_START_X + GAP + (BAR_WIDTH + GAP) * i, barY, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+
+    ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
     ctx.fillStyle = COLOR_BLACK;
-    ctx.fillText(names[i], CLOUD_START_X + GAP + (BAR_WIDTH + GAP) * i, PLAYER_NAME_Y);
-    ctx.fillText(parseInt(times[i], 10), CLOUD_START_X + GAP + (BAR_WIDTH + GAP) * i, PLAYER_NAME_Y - RESULT_GAP + (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillText(names[i], barX, PLAYER_NAME_Y);
+    ctx.fillText(parseInt(times[i], 10), barX, resultY);
   }
 };
